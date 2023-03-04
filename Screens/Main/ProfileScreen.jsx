8be1ab538cf05c7b349d89affcffb50d2c21bc7e
Dranyk <dvdranyk@gmail.com>
2023-03-04 13:47:22 +0200

    Update ProfileScreen.jsx

--- Screens/Main/ProfileScreen.jsx
+++ Screens/Main/ProfileScreen.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { useDispatch } from "react-redux";
+import { authSignOutUser } from "../../redux/auth/authOperations";
 
 const ProfileScreen = () => {
+  const dispatche = useDispatch();
+
+  const singOut = () => {
+    dispatche(authSignOutUser());
+  };
   return (
     <View style={styles.container}>
       <Text>ProfileScreen</Text>
-      <Button title="singOut"></Button>
+      <Button title="singOut" onPress={singOut}></Button>
     </View>
   );
 };
